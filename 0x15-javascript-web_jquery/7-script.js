af3ced2e3cc77445924e9e848d5d0017ab4b75ff
Adebusayo Opeyemi <91046730+Adebusayo0325@users.noreply.github.com>
2023-06-24 09:21:56 +0100

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/7-script.js
+++
@@ -1,6 +0,0 @@
-
-cument').ready(function () {
-  $.getJSON('https://swapi.co/api/people/5/?format=json', function (value) {
-    $('DIV#character').text(value.name);
-  });
-});
--- 0x15-javascript-web_jquery/README.md
+++
@@ -1,1 +0,0 @@
-Working wuth Jquery
